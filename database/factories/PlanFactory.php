16419399cbb8584c2778f0caf3be261860ac0b5f
Andreas Müller <mueller@mamedia-edv.de>
2019-04-30 12:10:19 +0200

    Compatibility update

--- database/factories/PlanFactory.php
+++ database/factories/PlanFactory.php
@@ -9,10 +9,11 @@
 | model instances for testing / seeding your application's database.
 |
 */
+use Illuminate\Support\Str;
 
 $factory->define(\Rennokki\Plans\Models\PlanModel::class, function () {
     return [
-        'name' => 'Testing Plan '.str_random(7),
+        'name' => 'Testing Plan '.Str::random(7),
         'description' => 'This is a testing plan.',
         'price' => (float) mt_rand(10, 200),
         'currency' => 'EUR',
--- tests/StripeHelperTest.php
+++ tests/StripeHelperTest.php
@@ -6,7 +6,7 @@ use Rennokki\Plans\Helpers\StripeHelper;
 
 class StripeHelperTest extends TestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
     }
